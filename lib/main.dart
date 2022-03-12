32b8d09eb6f820a8e07d661006d6933d62e4782c
TuanHazeeq <t.hazeeq@gmail.com>
2022-03-12 14:33:46 +0800

    Vimigo Task

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,30 +13,46 @@ Future<void> main() async {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  bool timeset = true;
   var message = "";
 
   late ScrollController? _scrollController = ScrollController();
 
-  @override
-  void initState(){
-    _scrollController = ScrollController()..addListener(() {
-      if(_scrollController!.offset >= _scrollController!.position.maxScrollExtent){
-        setState(() {
-          message = "Reach the bottom";
-        });
-      }else{
-        setState(() {
-          message = "";
-        });
-      }
+  _loadBool() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      timeset = prefs.getBool('option')!;
     });
+  }
+
+  _savebool() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('option', timeset);
+  }
+
+  @override
+  void initState() {
+    _loadBool();
+    _scrollController = ScrollController()
+      ..addListener(() {
+        if (_scrollController!.offset >=
+            _scrollController!.position.maxScrollExtent) {
+          setState(() {
+            message = "Reach the bottom";
+          });
+        } else {
+          setState(() {
+            message = "";
+          });
+        }
+      });
   }
 
   @override
@@ -44,46 +62,68 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           elevation: 0.0,
           title: const Text('Contact List'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.watch),
+              onPressed: () {
+                setState(() {
+                  timeset = !timeset;
+                  _savebool();
+                });
+              },
+            )
+          ],
         ),
         body: Column(
           children: [
             Container(
               height: 50.0,
               color: Colors.green,
-              child: Center(child: Text(message),),
+              child: Center(
+                child: Text(message),
+              ),
             ),
             Expanded(
               child: StreamBuilder(
-                stream: FirebaseFirestore.instance.collection('contacts').snapshots(),
-                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
-                  if(!snapshot.hasData){
+                stream: FirebaseFirestore.instance
+                    .collection('contacts')
+                    .orderBy('check-in', descending: true)
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (!snapshot.hasData) {
                     return const Center(
                       child: CircularProgressIndicator(),
                     );
                   }
-            
+
                   return ListView(
                     controller: _scrollController,
                     shrinkWrap: true,
-                  children: 
-                  snapshot.data!.docs.map((doc) {
-                    DateTime date = DateTime.parse(doc['check-in'].toDate().toString());
-                    var time = DateFormat('dd MMM yyyy hh:mm').format(date);
-                    return Center(
-                      child: ListTile(
-                        title: Text('Name: '+doc['user']),
-                        subtitle: Text('Phone Number: '+doc['phone']+'\nCheck-In: '+time.toString()),
+                    children: snapshot.data!.docs.map((doc) {
+                      DateTime date =
+                          DateTime.parse(doc['check-in'].toDate().toString());
+                      var ago = timeset
+                          ? timeago.format(date)
+                          : DateFormat('dd MMM yyyy hh:mm').format(date);
+                      return Center(
+                          child: ListTile(
+                        title: Text('Name: ' + doc['user']),
+                        subtitle: Text('Phone Number: ' +
+                            doc['phone'] +
+                            '\nCheck-In: ' +
+                            ago.toString()),
                         trailing: TextButton(
-                          child: const Text('Share'),
-                          onPressed: () async {
-                            await Share.share('Name:'+doc['user']+'\nPhone:'+doc['phone']);
-                          }
-                        ),
-                        
-                      )
-                    );
-                  }).toList(),
-                    );
+                            child: const Text('Share'),
+                            onPressed: () async {
+                              await Share.share('Name:' +
+                                  doc['user'] +
+                                  '\nPhone:' +
+                                  doc['phone']);
+                            }),
+                      ));
+                    }).toList(),
+                  );
                 },
               ),
             ),
@@ -93,4 +133,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
